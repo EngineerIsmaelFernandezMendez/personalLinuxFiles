6e7b228ca9da9c6e048d400df41983f210451836
Ismael Fernández Méndez <engineerismaelfernandezmendez@gmail.com>
2025-10-07 09:29:08 +0200

    First script for Ubuntu DistroBox: VSC.

---
+++ distrobox/ubuntu2404/installIDEs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#---VISUAL STUDIO CODE---
+#From https://code.visualstudio.com/docs/setup/linux#_install-vs-code-on-linux
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
+rm -f microsoft.gpg
+
+sudo tee /etc/apt/sources.list.d/vscode.sources > /dev/null <<'EOF'
+Types: deb
+URIs: https://packages.microsoft.com/repos/code
+Suites: stable
+Components: main
+Architectures: amd64,arm64,armhf
+Signed-By: /usr/share/keyrings/microsoft.gpg
+EOF
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
