1c3e4532c8270e48c949213fd4e3f4928b121b78
Ismael Fernández Méndez <engineerismaelfernandezmendez@gmail.com>
2026-04-26 18:00:44 +0200

    feat: Mongo CE script

---
+++ distrobox/ubuntu2404/installMongoCE.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Src: https://www.mongodb.com/docs/v7.0/tutorial/install-mongodb-on-ubuntu/
+sudo apt update
+# Install gnupg and curl if not installed
+sudo apt-get install gnupg curl
+
+# Import MongoDB public GPG key
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+
+# Create list file
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
